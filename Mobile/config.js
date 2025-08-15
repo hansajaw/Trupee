9b415e09e5afbfe7acd43eb14ce6b59c758d7949
Hansaja Wickrama <wickramahansaja@gmail.com>
2025-08-15 15:36:08 +0530

    Update BASE_URL to Railway domain

--- Mobile/config.js
+++ Mobile/config.js
@@ -4,4 +4,4 @@ const DEV_BASE =
   Platform.OS === "android" ? "http://172.20.10.6:3000" : "http://localhost:3000";
 
 
-export const BASE_URL = __DEV__ ? DEV_BASE : "https://your-prod-api.com";
+export const BASE_URL = __DEV__ ? DEV_BASE : "https://trupee-production.up.railway.app";
