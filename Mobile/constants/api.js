24d73d24aef1730862cfdb7e7add5f388fa8f388
Hansaja Wickrama <wickramahansaja@gmail.com>
2025-11-12 05:04:38 +0530

    Fixed some issues

--- Mobile/constants/api.js
+++ Mobile/constants/api.js
@@ -1,1 +1,2 @@
-export const API_URL = "https://trupee-18feqwj9z-hansaja-wickramas-projects.vercel.app/api"; 
+export const API_URL =
+  process.env.EXPO_PUBLIC_API_URL || "http://10.0.2.2:3000/api";
